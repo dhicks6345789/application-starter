53186a82cad5a368f59fbe95fd7e3448898ab528
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 13:45:08 +0100

    Update starter.go

--- starter.go
+++ starter.go
@@ -4,6 +4,9 @@ import "fmt"
 import "os/exec"
 
 func main() {
+  if _, err := os.Stat("G:\\My Drive"); os.IsNotExist(err) {
+    fmt.Println("Path does not exist!")
+  }
   err := exec.Command("C:\\Windows\\explorer.exe").Start()
   if err != nil {
     fmt.Println(err)
