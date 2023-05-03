00f52d1f37ec3505bfcc9bcaec7dbf2a726bb537
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 15:59:51 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,14 +2,14 @@
 echo Installing...
 
 erase starter.exe
-copy application-starter\go.mod .
-go get golang.org/x/sys/windows/registry
+rem copy application-starter\go.mod .
+rem go get golang.org/x/sys/windows/registry
 go build application-starter\starter.go
 
 if not exist "C:\Program Files\Application Starter" (
   mkdir "C:\Program Files\Application Starter"
 )
-rem copy /b/v/y starter.exe "C:\Program Files\Application Starter"
+copy /b/v/y starter.exe "C:\Program Files\Application Starter"
 rem starter.exe
 
 Set currentDir=%cd%
