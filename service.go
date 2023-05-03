5aad03b552151723f65eebd9aa37b8f8c3359a21
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 20:17:36 +0100

    Update service.go

--- service.go
+++ service.go
@@ -4,6 +4,7 @@ import (
   "fmt"
 	"log"
   "strings"
+  "os/exec"
   "net/http"
 )
 
@@ -11,6 +12,12 @@ func main() {
   http.HandleFunc("/", func (theResponseWriter http.ResponseWriter, theRequest *http.Request) {
     if strings.HasPrefix(theRequest.URL.Path, "/setExplorer") {
 		  fmt.Println("Handle setExplorer")
+      err := exec.Command("C:\\Windows\\regedit.exe", "/S", "C:\\Program Files\\Application Starter\\setExplorer.reg").Start()
+      if err != nil {
+        fmt.Fprint(theResponseWriter, err)
+      } else {
+        fmt.Fprint(theResponseWriter, "OK")
+      }
 	  }
   })
   log.Fatal(http.ListenAndServe("localhost:8090", nil))
