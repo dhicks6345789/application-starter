3d93631641335ab5bd4ec45502eb7ae243646782
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 14:26:56 +0100

    Update starter.go

--- starter.go
+++ starter.go
@@ -6,11 +6,13 @@ import "os"
 import "os/exec"
 
 func main() {
+  tries := 1
   _, pathErr := os.Stat("G:\\My Drive");
-  for os.IsNotExist(pathErr) {
+  for os.IsNotExist(pathErr) && tries < 5 {
     fmt.Println("Google Drive not ready yet.")
     time.Sleep(5 * time.Second)
     _, pathErr = os.Stat("G:\\My Drive");
+    tries = tries + 1
   }
   err := exec.Command("C:\\Windows\\explorer.exe").Start()
   if err != nil {
