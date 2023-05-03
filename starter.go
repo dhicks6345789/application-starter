27d2b4d959b23bd4da295ceff91d3e0464bac306
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 13:47:23 +0100

    Update starter.go

--- starter.go
+++ starter.go
@@ -1,12 +1,14 @@
 package main
 
 import "fmt"
+import "time"
 import "os"
 import "os/exec"
 
 func main() {
   if _, err := os.Stat("G:\\My Drive"); os.IsNotExist(err) {
-    fmt.Println("Path does not exist!")
+    fmt.Println("Google Drive not ready yet.")
+    time.Sleep(5 * time.Second)
   }
   err := exec.Command("C:\\Windows\\explorer.exe").Start()
   if err != nil {
