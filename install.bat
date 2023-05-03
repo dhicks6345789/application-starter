f30a55c8e3f7f5a3c0520fdf2ba9bb17602a59ca
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 14:29:59 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ go build application-starter\starter.go
 if not exist "C:\Program Files\Application Starter" (
   mkdir "C:\Program Files\Application Starter"
 )
-copy starter.exe "C:\Program Files\Application Starter"
+copy /b/v/y starter.exe "C:\Program Files\Application Starter"
 rem starter.exe
 
 Set currentDir=%cd%
