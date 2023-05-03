a4a21c53c2ec41e5d3b36401e9e4288a5cc67aa6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 20:06:45 +0100

    Update service.go

--- service.go
+++ service.go
@@ -2,13 +2,14 @@ package main
 
 import (
   "fmt"
+  "strings"
   "net/http"
 )
 
 func main() {
   http.HandleFunc("/", func (theResponseWriter http.ResponseWriter, theRequest *http.Request) {
-	  if strings.HasPrefix(theRequest.URL.Path, "/setExplorer") {
-		  fmt.println("Handle setExplorer")
+    if strings.HasPrefix(theRequest.URL.Path, "/setExplorer") {
+		  fmt.Println("Handle setExplorer")
 	  }
   })
 }
