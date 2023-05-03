e9c1b82ca6fe930a0e1f816b744b0bff95915a1e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 19:48:43 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -17,12 +17,12 @@ echo Installing...
 if not exist "C:\Program Files\Application Starter" (
   mkdir "C:\Program Files\Application Starter"
 )
-copy /b/v/y starter.exe "C:\Program Files\Application Starter"
+copy /y starter.exe "C:\Program Files\Application Starter"
 erase starter.exe
-copy /b/v/y service.exe "C:\Program Files\Application Starter"
+copy /y service.exe "C:\Program Files\Application Starter"
 erase service.exe
-copy /b/v/y application-starter\setExplorer.reg "C:\Program Files\Application Starter"
-copy /b/v/y application-starter\setStarter.reg "C:\Program Files\Application Starter"
+copy /y application-starter\setExplorer.reg "C:\Program Files\Application Starter"
+copy /y application-starter\setStarter.reg "C:\Program Files\Application Starter"
 
 echo Setting up Windows service...
 application-starter\nssm\2.24\win64\nssm install ApplicationStarter "C:\Program Files\Application Starter\service.exe" > nul 2>&1
