12193591077475bfb4017dceebc33401e0721c68
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 14:14:27 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,7 +3,12 @@ echo Installing...
 
 erase starter.exe
 go build application-starter\starter.go
-starter.exe
+
+if not exist "C:\Program Files\Application Starter" (
+  mkdir "C:\Program Files\Application Starter"
+)
+copy starter.exe "C:\Program Files\Application Starter"
+rem starter.exe
 
 Set currentDir=%cd%
 regedit /S %currentDir%\application-starter\settings.reg
