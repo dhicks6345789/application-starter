f7c775ec7dc6218bfda83ff08505689a1ee54fa5
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 19:29:13 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -11,6 +11,13 @@ copy /b/v/y starter.exe "C:\Program Files\Application Starter"
 copy /b/v/y application-starter\setExplorer.reg "C:\Program Files\Application Starter"
 copy /b/v/y application-starter\setStarter.reg "C:\Program Files\Application Starter"
 
+echo Setting up Windows service...
+application-starter\nssm-2.24\win64\nssm install ApplicationStarter "C:\Program Files\Application Starter\service.exe" > nul 2>&1
+application-starter\nssm-2.24\win64\nssm set ApplicationStarter DisplayName "Application Starter" > nul 2>&1
+application-starter\nssm-2.24\win64\nssm set ApplicationStarter AppNoConsole 1 > nul 2>&1
+application-starter\nssm-2.24\win64\nssm set ApplicationStarter Start SERVICE_AUTO_START > nul 2>&1
+net start ApplicationStarter
+
 Set currentDir=%cd%
 regedit /S %currentDir%\application-starter\settings.reg
 
