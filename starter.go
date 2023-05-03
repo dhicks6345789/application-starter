b4b38dbe6b145fefa3afa11effe746d5414341f7
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-03 13:36:48 +0100

    Update starter.go

--- starter.go
+++ starter.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "exec"
 
 func main() {
   fmt.Println("Hello, world!")
+  err := exec.Command("C:\\Windows\\explorer.exe").Run()
+  if err != nil {
+    log.Fatal(err)
+  }
 }
