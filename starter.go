192eb1415a6308bb47181677dc4352169bbcc052
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-04 18:52:13 +0100

    Update starter.go

--- starter.go
+++ starter.go
@@ -16,6 +16,16 @@ func debug(theMessage string) {
   }
 }
 
+func callEndpoint(theEndpoint string) {
+  resp, err := http.Get(theEndpoint)
+  if err != nil {
+    debug(err.Error())
+  }
+  body, err := ioutil.ReadAll(response.Body)
+  debug(body)
+  resp.Body.Close()
+}
+
 func main() {
   // Make sure Google Drive is started.
   err := exec.Command("C:\\Program Files\\Google\\Drive File Stream\\launch.bat").Start()
@@ -25,7 +35,7 @@ func main() {
   
   // Set the Shell registry value temporarily back to "Explorer.exe" so that Windows Explorer starts in "shell" mode, displaying the desktop, taskbar and
   // so on. For this we need to have elevated privalages, so we ask a service running as the system user to do the operation.
-  _, _ = http.Get("http://localhost:8090/setExplorer")
+  callEndpoint("http://localhost:8090/setExplorer")
   
   // Wait for Google Drive to be ready.
   tries := 1
@@ -39,9 +49,9 @@ func main() {
   // Start Windows Explorer to display the desktop.
   err = exec.Command("C:\\Windows\\explorer.exe").Start()
   if err != nil {
-    fmt.Println(err)
+    debug(err.Error())
   }
   
   // Set the Shell registry value back to this application.
-  _, _ = http.Get("http://localhost:8090/setStarter")
+  callEndpoint("http://localhost:8090/setStarter")
 }
