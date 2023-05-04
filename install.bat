7535133bf7bacfdb899ff0f3f4bdf8d296bc6a24
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-04 17:55:40 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -13,18 +13,10 @@ shift
 goto paramLoop
 :paramContinue
 
-echo bananas
-if "%debug%"==1 (
-  echo grapes
-)
-echo oranges
-
 echo Compiling Go code...
 if "%debug%"==1 (
-  echo one
   go build -ldflags "-X main.debugOn=true" application-starter\starter.go
 ) else (
-  echo two
   go build -ldflags -H=windowsgui "-X main.debugOn=false" application-starter\starter.go
 )
 if not exist starter.exe (
