fd914de03d4895009f1ab5e53f9bd42f1978afa0
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-04 15:00:51 +0100

    Update service.go

--- service.go
+++ service.go
@@ -41,7 +41,7 @@ func main() {
             userID := strings.TrimSpace(userSplit[1])
             if userID != ".DEFAULT" && !strings.HasSuffix(userID, "_Classes") {
               if _, pathErr := os.Stat("C:\\Program Files\\Application Starter\\Users\\" + userID); errors.Is(pathErr, os.ErrNotExist) {
-                fileWriteErr := os.WriteFile("C:\\Program Files\\Application Starter\\Users\\" + userID, []byte(strings.ReplaceAll(perUserString, "HKEY_CURRENT_USER\\", "HKEY_USERS\\" + userID + "\\")))
+                fileWriteErr := os.WriteFile("C:\\Program Files\\Application Starter\\Users\\" + userID, []byte(strings.ReplaceAll(perUserString, "HKEY_CURRENT_USER\\", "HKEY_USERS\\" + userID + "\\")), 0644)
                 if fileWriteErr != nil {
                   fmt.Printf("Error writing file:: %s\n", "C:\\Program Files\\Application Starter\\Users\\" + userID)
                 } else {
