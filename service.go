7dd1724c0b40bd55dc4ef341c4eb0693f783c6bc
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-04 13:47:49 +0100

    Update service.go

--- service.go
+++ service.go
@@ -16,15 +16,17 @@ func main() {
       cmd := exec.Command("C:\\Windows\\System32\\reg.exe", "Query", "HKEY_USERS")
       out, err := cmd.CombinedOutput()
       if err != nil {
-        fmt.Printf("cmd.Run() failed with %s\n", err)
-      }
-      fmt.Printf("Combined out:\n%s\n", string(out))
-      
-      err = exec.Command("C:\\Windows\\regedit.exe", "/S", "C:\\Program Files\\Application Starter\\setExplorer.reg").Start()
-      if err != nil {
-        fmt.Fprint(theResponseWriter, err)
+        fmt.Printf("Query to registry failed: %s\n", err)
       } else {
-        fmt.Fprint(theResponseWriter, "OK")
+        for _, user := range strings.split(string(out), "\n") {
+          fmt.Println(user)
+        }
+        err = exec.Command("C:\\Windows\\regedit.exe", "/S", "C:\\Program Files\\Application Starter\\setExplorer.reg").Start()
+        if err != nil {
+          fmt.Fprint(theResponseWriter, err)
+        } else {
+          fmt.Fprint(theResponseWriter, "OK")
+        }
       }
 	  }
     if strings.HasPrefix(theRequest.URL.Path, "/setStarter") {
