e47be87bac87cac2ee472d07c5d89af06af1c56e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-10 15:26:54 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -53,6 +53,7 @@ rem   del /S /Q "C:\Program Files\Application Starter\Users\*"
 rem )
 
 copy /y starter.exe "C:\Program Files\Application Starter"
+copy /y starter.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 erase starter.exe
 rem copy /y service.exe "C:\Program Files\Application Starter"
 rem erase service.exe
