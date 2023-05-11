44eddf7dfa9901e450b33114ad2d55518cca1754
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-11 09:26:24 +0100

    Update starter.go

--- starter.go
+++ starter.go
@@ -23,18 +23,16 @@ func main() {
     os.Exit(0)
   }
   
+  // If this is a user's first run, we need to quit so the first run application can run instead.
   if _, pathErr := os.Stat(userHome + "\\AppData\\Local\\ApplicationStarter"); os.IsNotExist(pathErr) {
     os.Exit(0)
   }
   
   // Pause so Explorer has time to start properly.
-  time.Sleep(2 * time.Second)
+  //time.Sleep(2 * time.Second)
   
   // Stop Windows Explorer.
   _ = exec.Command("C:\\Windows\\System32\\Taskkill.exe", "/f", "/im", "explorer.exe").Run()
-  
-  // Set user folder redirects.
-  _ = exec.Command("C:\\Windows\\regedit.exe", "/S", "C:\\Program Files\\Application Starter\\setPerUser.reg").Run()
   
   // Check if Google Drive is ready by checking for G:\My Drive...
   tries := 1
@@ -44,21 +42,11 @@ func main() {
     debug("Starting Google Drive...")
     _ = exec.Command("C:\\Program Files\\Google\\Drive File Stream\\launch.bat").Start()
   }
-  // ...and wait for it to be ready...
+  // ...and wait for it to be ready.
   for pathErr != nil && tries < 60 {
     debug("Google Drive not ready yet.")
     time.Sleep(1 * time.Second)
     _, pathErr = os.Stat("G:\\My Drive");
-    tries = tries + 1
-  }
-  // ...and wait for G:\My Drive\Desktop to be ready...
-  tries = 1
-  _, pathErr = os.Stat("G:\\My Drive\\Desktop");
-  for pathErr != nil && tries < 60 {
-    debug("Desktop folder not ready yet.")
-    _ = os.Mkdir("G:\\My Drive\\Desktop", 0750)
-    time.Sleep(1 * time.Second)
-    _, pathErr = os.Stat("G:\\My Drive\\Desktop");
     tries = tries + 1
   }
   
