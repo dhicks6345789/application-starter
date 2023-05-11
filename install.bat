aa6e91afee8112cc1c03a7abd505ed7da2d051e7
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-11 09:11:52 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -48,7 +48,7 @@ if not exist firstRun.exe (
   echo Compile fail - firstRun.go
   exit /B 1
 )
-rem copy /y firstRun.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+copy /y firstRun.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 erase firstRun.exe
 
 rem go build application-starter\service.go
