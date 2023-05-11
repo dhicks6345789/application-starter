5963c65928a48c9409f981e6373303a61ccb6270
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-11 10:28:41 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -73,6 +73,7 @@ rem erase service.exe
 copy /y application-starter\setExplorer.reg "C:\Program Files\Application Starter"
 copy /y application-starter\setStarter.reg "C:\Program Files\Application Starter"
 copy /y application-starter\setPerUser.reg "C:\Program Files\Application Starter"
+copy /y application-starter\setUserRun.reg "C:\Program Files\Application Starter"
 
 rem echo Setting up Windows service...
 rem application-starter\nssm\2.24\win64\nssm install ApplicationStarter "C:\Program Files\Application Starter\service.exe" > nul 2>&1
