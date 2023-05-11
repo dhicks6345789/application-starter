5c6818413060b72db405fa934a06d4bcb56d3dbe
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-05-11 10:49:35 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -49,49 +49,14 @@ if not exist firstRun.exe (
   echo Compile fail - firstRun.go
   exit /B 1
 )
-rem copy /y firstRun.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-erase "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\firstRun.exe"
-rem copy /y firstRun.exe "C:\Program Files\Application Starter"
+copy /y firstRun.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 erase firstRun.exe
 
-rem go build application-starter\service.go
-rem if not exist service.exe (
-  rem echo Compile fail - service.go
-  rem exit /B 1
-rem )
-
-rem echo Stopping existing service...
-rem net stop ApplicationStarter
-
-
-rem if %debug%==1 (
-rem   del /S /Q "C:\Program Files\Application Starter\Users\*"
-rem )
-
-rem copy /y service.exe "C:\Program Files\Application Starter"
-rem erase service.exe
 copy /y application-starter\setExplorer.reg "C:\Program Files\Application Starter"
 copy /y application-starter\setStarter.reg "C:\Program Files\Application Starter"
 copy /y application-starter\setPerUser.reg "C:\Program Files\Application Starter"
-copy /y application-starter\setUserRun.reg "C:\Program Files\Application Starter"
-
-rem echo Setting up Windows service...
-rem application-starter\nssm\2.24\win64\nssm install ApplicationStarter "C:\Program Files\Application Starter\service.exe" > nul 2>&1
-rem application-starter\nssm\2.24\win64\nssm set ApplicationStarter Description "A shell replacement for Explorer that starts up Google Drive then Explorer, letting users be able to redirect their desktop folders to Google Drive." > nul 2>&1
-rem application-starter\nssm\2.24\win64\nssm set ApplicationStarter DisplayName "Application Starter" > nul 2>&1
-rem application-starter\nssm\2.24\win64\nssm set ApplicationStarter AppNoConsole 1 > nul 2>&1
-rem application-starter\nssm\2.24\win64\nssm set ApplicationStarter Start SERVICE_AUTO_START > nul 2>&1
-
-rem echo Starting service...
-rem net start ApplicationStarter
 
 Set currentDir=%cd%
 regedit /S %currentDir%\application-starter\settings.reg
-
-rem if %debug%==1 (
-rem   echo Running Application Starter in debug mode...
-rem   net stop ApplicationStarter
-rem   "C:\Program Files\Application Starter\service.exe" --debug
-rem )
 
 echo Done!
